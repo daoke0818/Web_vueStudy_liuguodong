bf0d920e508311cec2180d317fbc432176a582c8
daooke0818 <daoke_li@qq.com>
2019-01-26 23:47:03 +0800

    watch属性

--- js/02_dataCalculation.js
+++ js/02_dataCalculation.js
@@ -5,23 +5,56 @@ let app = new Vue({
     }
 });
 let app2 = new Vue({
-    el:'#app2',
-   data:{
-        books:[
-            {name:'javaScript',number:2,price:35},
-            {name:'java',number:1,price:45},
-            {name:'设计模式之禅',number:2,price:50},
-            {name:'编程大法',number:10,price:24},
+    el: '#app2',
+    data: {
+        soul:0,
+        testCounter: 0,
+        books: [
+            {name: 'javaScript', number: 2, price: 35},
+            {name: 'java', number: 1, price: 45},
+            {name: '设计模式之禅', number: 2, price: 50},
+            {name: '编程大法', number: 10, price: 24},
         ]
-   },
-    methods:{
-        totalPrice:function () {
+    },
+    methods: {
+        domChange: function () {
+            console.log('调用 domChange');
+            this.testCounter++;
+        },
+        totalPrice: function () {
+            // console.log('调用 totalPrice','soul:'+this.soul++);
+            console.log('调用 totalPrice');
+
             let total = 0;
-            this.books.forEach(book=>{
-                total+= book.number*book.price;
+            this.books.forEach(book => {
+                total += book.number * book.price;
             });
             return total;
         }
+    }
+});
+let app3 = new Vue({
+    el: '#app3',
+    data: {
+        books: [
+            {name: 'javaScript', number: 2, price: 35},
+            {name: 'java', number: 1, price: 45},
+            {name: '设计模式之禅', number: 2, price: 50},
+            {name: '编程大法', number: 10, price: 24},
+        ],
+        totalPrice:0.00
+    },
+    watch: {
+        books: {
+            handler:function (val,oldVar) {
+                alert(1)
+                this.totalPrice = 0;
+                val.forEach(book => {
+                    this.totalPrice += book.number * book.price;
+                });
+            }
+        },
+        deep:true
     }
 });
 
