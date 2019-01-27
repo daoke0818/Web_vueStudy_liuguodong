bf272d407b240a2d2b462d54bb9f97a676601812
daoke0818 <gdddd9678>
2019-01-27 23:15:54 +0800

    计算属性

--- js/02_dataCalculation.js
+++ js/02_dataCalculation.js
@@ -7,7 +7,7 @@ let app = new Vue({
 let app2 = new Vue({
     el: '#app2',
     data: {
-        soul:0,
+        soul: 0,
         testCounter: 0,
         books: [
             {name: 'javaScript', number: 2, price: 35},
@@ -43,9 +43,9 @@ let app3 = new Vue({
             {name: '设计模式之禅', number: 2, price: 50},
             {name: '编程大法', number: 10, price: 24},
         ],
-        totalPrice:0.00
+        totalPrice: 0.00
     },
-    methods:{
+    methods: {
         domChange: function () {
             console.log('调用 domChange');
             this.testCounter++;
@@ -54,19 +54,94 @@ let app3 = new Vue({
 
     watch: {
         books: {
-            handler:function (val,oldVar) {
+            handler: function (val, oldVar) {
                 console.log('调用 totalPrice');
                 this.totalPrice = 0;
                 val.forEach(book => {
                     this.totalPrice += book.number * book.price;
                 });
             },
-            deep:true
+            deep: true
         },
-        // deep:true
     }
 });
+let app4 = new Vue({
+    el: '#app4',
+    data: {
+        books: [
+            {name: 'javaScript', number: 2, price: 35},
+            {name: 'java', number: 1, price: 45},
+            {name: '设计模式之禅', number: 2, price: 50},
+            {name: '编程大法', number: 10, price: 24},
+        ],
+        totalPrice: 0
+    },
+    watch: {
+        books: {
+            handler: function (val, oldval) {
+                let _this = this;
+                this.totalPrice = '计算中。。。';
+                setTimeout(function () {
+                    _this.totalPrice = 0;
+                    val.forEach(book => {
+                        _this.totalPrice += book.number * book.price;
+                    })
+                }, 1000)
 
+            },
+            deep: true
+        }
+    }
+});
+let app5 = new Vue({
+    el: '#app5',
+    data: {
+        firstName: 'Li',
+        lastName: 'Yingjie',
+        fullName: 'Li Yingjie'
+    },
+    watch: {
+        firstName: function (val) {
+            this.fullName = val + ' ' + this.lastName;
+        },
+        lastName: function (val) {
+            this.fullName = this.firstName + ' ' + val;
+        }
+    }
+});
+let app6 = new Vue({
+    el: '#app6',
+    data: {
+        firstName: 'Li',
+        lastName: 'Yingjie',
+    },
+    computed: {
+        fullName: function () {
+            return this.fullName = this.firstName + ' ' + this.lastName;
+        }
+    }
+});
+let app7 = new Vue({
+    el: '#app7',
+    data: {
+        books: [
+            {name: 'javaScript', number: 2, price: 35},
+            {name: 'java', number: 1, price: 45},
+            {name: '设计模式之禅', number: 2, price: 50},
+            {name: '编程大法', number: 10, price: 24},
+        ],
+    },
+    computed: {
+        totalPrice: function () {
+            let total = 0;
+            this.books.forEach(book => {
+                total += book.number * book.price;
+            });
+            return total;
+        }
+
+    }
+});
 
 
 
