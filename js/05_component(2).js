6a05b9d53b09836fe4b3502bb9415d72c61d2ec7
daoke0818 <daoke_li@qq.com>
2019-01-31 23:33:00 +0800

    父子组件传事件

---
+++ js/05_component(2).js
@@ -0,0 +1,57 @@
+Vue.component('app-main-view',{
+    template:'#app-main-view',
+    data:function () {
+        return {
+            pushSubDescObj:undefined
+        }
+    },
+    methods:{
+        onChangeDescClick:function (val) {
+            this.pushSubDescObj = {
+                title:val,
+                desc:'这是'+val+'的描述信息'
+            }
+        },
+        onDescFontSizeClick:function () {
+            this.$root.$emit('descFontSizeAdd',1)
+        }
+    }
+});
+Vue.component('app-desc-view',{
+    template:'#app-desc-view',
+    props:{
+        pushSubDescObj:{
+            type:Object,
+            default:function () {
+                return {
+                    title: '标题',
+                    desc:'描述内容'
+                }
+            }
+
+        }
+    },
+    data:function(){
+        return {fontSize:12}
+    },
+    created(){
+        let $this = this;
+        this.$root.$on('descFontSizeAdd',function (num) {
+            // alert($this.fontSize)
+            $this.fontSize += num;
+        })
+    }
+});
+let app = new Vue({
+    el:'#app'
+});
+
+
+
+
+
+
+
+
+
+
