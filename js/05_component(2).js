f07b6fba23ced6f21a6a7786e7deb588a33f1438
daoke0818 <daoke_li@qq.com>
2019-02-02 23:04:08 +0800

    插槽

--- js/05_component(2).js
+++ js/05_component(2).js
@@ -1,51 +1,55 @@
-Vue.component('app-main-view',{
-    template:'#app-main-view',
-    data:function () {
+Vue.component('app-main-view', {
+    template: '#app-main-view',
+    data: function () {
         return {
-            pushSubDescObj:undefined
+            pushSubDescObj: undefined
         }
     },
-    methods:{
-        onChangeDescClick:function (val) {
+    methods: {
+        onChangeDescClick: function (val) {
             this.pushSubDescObj = {
-                title:val,
-                desc:'这是'+val+'的描述信息'
+                title: val,
+                desc: '这是' + val + '的描述信息'
             }
         },
-        onDescFontSizeClick:function () {
-            this.$root.$emit('descFontSizeAdd',1)
+        onDescFontSizeClick: function () {
+            this.$root.$emit('descFontSizeAdd', 1)
         }
     }
 });
-Vue.component('app-desc-view',{
-    template:'#app-desc-view',
-    props:{
-        pushSubDescObj:{
-            type:Object,
-            default:function () {
+Vue.component('app-desc-view', {
+    template: '#app-desc-view',
+    props: {
+        pushSubDescObj: {
+            type: Object,
+            default: function () {
                 return {
                     title: '标题',
-                    desc:'描述内容'
+                    desc: '描述内容'
                 }
             }
-
         }
     },
-    data:function(){
-        return {fontSize:12}
+    data: function () {
+        return {fontSize: 12}
     },
-    created(){
+    created() {
         let $this = this;
-        this.$root.$on('descFontSizeAdd',function (num) {
+        this.$root.$on('descFontSizeAdd', function (num) {
             // alert($this.fontSize)
             $this.fontSize += num;
         })
     }
 });
 let app = new Vue({
-    el:'#app'
+    el: '#app'
 });
-
+Vue.component('app2-main-view',{
+    template:'<div><slot></slot></div>'
+});
+let app2 = new Vue({
+   el:'#app2'
+});
 
 
 
