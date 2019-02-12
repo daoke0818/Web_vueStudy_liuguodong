56a109a528dea43b940adad353954b04190285f8
daoke0818 <daoke_li@qq.com>
2019-02-12 22:26:23 +0800

    嵌套路由完，代码等部分样式

--- js/06_router.js
+++ js/06_router.js
@@ -20,3 +20,75 @@ const app = new Vue({
     el:"#app"
 });
 
+const app2One = {
+    template:'<p>这是one视图</p>'
+};
+const app2User = {
+    template:`<div>
+            <p>这是user视图</p>
+            <p>$router.params: {{$route.params}}</p>
+            <p>$router.query: {{$route.query}}</p>
+        </div>`
+};
+const app2Routes = [{
+    path:'/one',
+    component:app2One
+},{
+    path:'/user/:id',
+    name:'user',
+    component:app2User
+}];
+const app2router = new VueRouter({
+    routes:app2Routes,
+    mode: 'history'
+});
+const app2 = new Vue({
+    router:app2router,
+    el:'#app2',
+    methods:{
+        toNameClick:function () {
+            this.$router.push({
+                name:'user',
+                params:{
+                    id:'HaDi'
+                }
+            });
+        },
+        toPathClick:function () {
+            this.$router.push({
+                path:'/user/Black?id=MaDan',
+                query:{
+                    weight:3.6
+                }
+            });
+        }
+    }
+});
+
+const app3sup = {
+    template:'#app3-sup'
+};
+const app3Sub1 = {
+    template:'<div>这是sub1内容</div>'
+};
+const app3Sub2 = {
+    template:'<div>这是sub2内容</div>'
+};
+const app3Routes = [{
+    path:'/',
+    component:app3sup,
+    children:[{
+        path:'sub1',
+        component:app3Sub1
+    },{
+        path:'sub2',
+        component:app3Sub2
+    }]
+}];
+const app3Router = new VueRouter({
+    routes:app3Routes
+});
+const app3 = new Vue({
+    el:'#app3',
+    router:app3Router
+});
--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 $(function () {
     $('ul.mySummary').addClass('my-3 p-2 pl-5 border border-warning');
-
+    $('.container h1').after('<p>运行环境：vue-2.5.17.js，Chrome71</p>')
 });
