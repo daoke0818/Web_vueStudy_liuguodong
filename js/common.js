64738273f5e9796b8b01dedca4a3b9c814c08dae
daoke0818 <daoke_li@qq.com>
2019-08-22 16:06:56 +0800

    添加导读，返回目录按钮

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 $(function () {
     $('ul.mySummary').addClass('my-3 p-2 pl-5 border border-warning');
-    $('.container h1').after('<p>运行环境：vue-2.5.17.js，Chrome71</p>')
+    $('.container h1').after('<p class="d-flex justify-content-between">运行环境：vue-2.5.17.js，Chrome71+ <a href="../index.html">目录</a></p>')
 });
